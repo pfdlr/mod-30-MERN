010ba0ba6b6b395576863f4b281456285c4670f2
Piotr Fidler <fidler.piotr@gmail.com>
2019-10-29 12:19:45 +0100

    add mongodb

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const config = require('./config');
+const mongoose = require('mongoose');
+
+const app = express();
+
 const postRoutes = require('./routes/post.routes');
+
+mongoose.connect(config.DB, { useNewUrlParser: true });
+let db = mongoose.connection; 
+db.once('open', () => console.log('Connected to the database'));
+db.on('error', (err) => console.log('Error ' + err));
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
