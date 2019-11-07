dbc2eaf65ca3e68c0cf88eef6006dea2341cee89
Piotr Fidler <fidler.piotr@gmail.com>
2019-11-07 14:48:02 +0100

    add facebook comments plugin

--- client/src/components/features/SinglePost/SinglePost.js
+++ client/src/components/features/SinglePost/SinglePost.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { FacebookProvider, Comments, ShareButton } from 'react-facebook';
+import { withRouter } from 'react-router-dom';
+
 import { PropTypes } from 'prop-types';
 import Spinner from '../../common/Spinner/Spinner';
 import Alert from '../../common/Alert/Alert';
 import PageTitle from '../../common/PageTitle/PageTitle';
 import HtmlBox from '../../common/common/HtmlBox/HtmlBox';
+import { BASE_URL } from '../../../config.js'
 
 
 class SinglePost extends React.Component {
@@ -15,14 +19,20 @@ class SinglePost extends React.Component {
   }
 
   render() {
-    const { posts, request } = this.props;
+    const { posts, request, location } = this.props;
 
     if (request.pending === false && request.success === true && posts.length > 0)
       return (
         <div>
-          <PageTitle>{ posts[0].title }</PageTitle>
-          <p>Author: { posts[0].author}</p>
-          <HtmlBox>{ posts[0].content }</HtmlBox>
+          <PageTitle>{posts[0].title}</PageTitle>
+          <p>Author: {posts[0].author}</p>
+          <HtmlBox>{posts[0].content}</HtmlBox>
+          <FacebookProvider appId="757076174720337">
+            <Comments href={`${BASE_URL}/${location.pathname}`} />
+            <ShareButton href="http://www.facebook.com">
+              Share
+            </ShareButton>
+          </FacebookProvider>
         </div>
       );
     else if (request.pending === true || request.success === null)
@@ -53,4 +63,4 @@ SinglePost.propTypes = {
   loadPost: PropTypes.func.isRequired,
 };
 
-export default SinglePost;
+export default withRouter(props => <SinglePost {...props} />);
