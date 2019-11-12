cea855000c7c81880cbd1a34e538c998b6fae619
Piotr Fidler <fidler.piotr@gmail.com>
2019-11-12 11:20:24 +0100

    change config

--- server/server.js
+++ server/server.js
@@ -29,12 +29,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api', postRoutes);
+
 app.use(express.static(path.join(__dirname, '/../client/build'))); // Serve static files from the React app
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
-
+app.use('/api', postRoutes);
 
 app.listen(config.PORT, function () {
     console.log('Server is running on port:', config.PORT);
