539b24607114c749f0fec64bc70968b94f5a12e9
Piotr Fidler <fidler.piotr@gmail.com>
2019-11-15 08:56:59 +0100

    add build

---
+++ client/build/precache-manifest.c58b26ad67d6c2c9859eaada99e9f89f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3586b73eeb25c912bec43f6fbd290f86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13f4ed7e8172797b433a",
+    "url": "/static/css/2.b569b48b.chunk.css"
+  },
+  {
+    "revision": "3ec635e836f783d1dda9",
+    "url": "/static/css/main.cc3ef343.chunk.css"
+  },
+  {
+    "revision": "13f4ed7e8172797b433a",
+    "url": "/static/js/2.c0e853a7.chunk.js"
+  },
+  {
+    "revision": "3ec635e836f783d1dda9",
+    "url": "/static/js/main.f719fd54.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
